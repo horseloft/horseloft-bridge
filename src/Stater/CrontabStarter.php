65aa265c7fce6acdb6046ab66dae688819cacc8f
YHC <yanhaochuang@qq.com>
2022-09-15 16:30:37 +0800

    新增类文件用于处理定时任务相关

--- src/Stater/CrontabStarter.php
+++ src/Stater/CrontabStarter.php
@@ -1,13 +1,39 @@
 <?php
-/**
- * Date: 2022/9/15 14:44
- * User: YHC
- * Desc:
- */
 
 namespace Horseloft\Phalanx\Stater;
 
-class CrontabStarter
-{
+use Horseloft\Phalanx\Handler\Crontab;
 
+trait CrontabStarter
+{
+    /**
+     * 定时任务
+     *
+     * @param array $crontabList
+     */
+    private function scheduleRun(array $crontabList)
+    {
+        $minute = -1;
+        $isRecovered = false;
+        $crontab = new Crontab();
+        while (true)
+        {
+            // 分钟数相同 并且已经运行过
+            if ($minute == date('i') && $isRecovered) {
+                sleep(1);
+                continue;
+            }
+
+            $minute = date('i');
+            $task = $crontab->runTask($crontabList);
+            $isRecovered = true;
+
+            // 没有任务执行，未创建进程
+            if (empty($task)) {
+                continue;
+            }
+            // 任务检测
+            $crontab->processObserver();
+        }
+    }
 }
